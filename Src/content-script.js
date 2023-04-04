dd0d50378c3f64d227f8b97c23d574b89ac67629
Jaraxxxus <72243384+Jaraxxxus@users.noreply.github.com>
2023-04-04 20:37:05 +0700

    init

---
+++ Src/content-script.js
@@ -0,0 +1,28 @@
+console.log('hey-hey')
+const preEls = document.querySelectorAll('pre');
+[...preEls].forEach(preEl =>{
+
+    const root = document.createElement("div");
+    root.style.position = "relative";
+    const shadowRoot = root.attachShadow({mode : 'open'});
+
+    const cssUrl = chrome.runtime.getURL("contentscript.css");
+
+    shadowRoot.innerHTML = '<link rel="stylesheet" type="text/css" href="' + cssUrl + '"></link>';
+
+
+    const button = document.createElement("button");
+    button.innerText = 'Copy';
+    button.type = 'button';
+    shadowRoot.prepend(button);
+
+    preEl.prepend(root);
+
+    const codeEl = preEl.querySelector('code');
+
+    preEl.prepend(button);
+    button.addEventListener('click', () => {
+        navigator.clipboard.writeText(codeEl.innerText)
+        console.log('hey-hey')
+    });
+})
